ee5a6a0db5581af76a5975042ab01435843956b7
Carlos Alvarez <cead22@gmail.com>
2014-10-10 18:28:24 +0100

    Add first passing test!!

---
+++ segmentedControl.js
@@ -0,0 +1,59 @@
+$.widget( 'expensify.segmentedControl', {
+
+    _onChangeCallback: null,
+
+    _create: function(){
+        this.element.find( 'li:first-child' ).addClass( 'active' );
+
+        this._on({
+            click: this._onClick
+        });
+    },
+
+    _onClick: function( event ){
+        event.preventDefault();
+
+        // Do nothing if this control is disabled
+        if( this.element.hasClass( 'disabled' ) ){
+            return;
+        }
+
+        var $clickedItem = $(event.target);
+
+        // Do nothing if the active element is clicked
+        if( $clickedItem.hasClass( 'active' ) ){
+            return;
+        }
+
+        // "Activate" the new item
+        this.element.find( 'li.active' ).removeClass( 'active' );
+        $clickedItem.addClass( 'active' );
+
+        // Invoke the callback if we have one
+        if( typeof this._onChangeCallback === 'function' ){
+            this._onChangeCallback();
+        }
+    },
+
+    _destroy: function(){
+        this.element.find( 'li' ).removeClass( 'active' );
+        this._off( this.element, 'click' );
+    },
+
+    getValue: function(){
+        return this.element.find( 'li.active' ).attr( 'data-value' );
+    },
+
+    getActiveLabel: function(){
+        return this.element.find( 'li.active' ).text();
+    },
+
+    setValue: function( newValue ){
+        this.element.find( 'li' ).removeClass( 'active' );
+        this.element.find( 'li[data-value="' + newValue + '"]' ).addClass( 'active' );
+    },
+
+    setOnChange: function( callback ){
+        this._onChangeCallback = callback;
+    }
+});
---
+++ test/segmentedControlTest.js
@@ -0,0 +1,27 @@
+// jQuery will throw an error without these
+jsdom = require( 'jsdom' ).jsdom;
+document = jsdom('<html><head><script></script></head><body>\
+                    <ul id="people_filter_type" class="segmented-control">\
+                        <li data-value="all" class="active">All</li>\
+                        <li data-value="approvers">Approvers</li>\
+                        <li data-value="admins">Administrators</li>\
+                    </ul>\
+                </body></html>');
+
+window = document.parentWindow;
+navigator = window.navigator = {};
+navigator.userAgent = 'NodeJs JsDom';
+
+chai = require( 'chai' );
+assert = chai.assert;
+$ = require( 'jquery' );
+require( 'jquery-ui' );
+require( '../segmentedControl.js' );
+
+describe( 'Segmented Control', function(){
+    var control = $( 'ul' ).segmentedControl();
+
+    it( 'Sets the value to the first LI element in the UL by default', function(){
+        assert.equal( control.segmentedControl( 'getValue' ), 'all' );
+    });
+});
